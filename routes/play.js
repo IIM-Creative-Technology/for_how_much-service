da087054ff77ab487d84fa4f0c7018999b3fde0f
Edouard Nicolas <edouard.n@lifeaz.fr>
2022-10-13 13:42:33 +0200

    Changes by Romain

--- routes/play.js
+++ routes/play.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Challenge listing. */
-router.get('/Challenge', function(req, res, next) {
+router.get('/challenge', function(req, res, next) {
   res.send('respond with a resource');
 });
 
