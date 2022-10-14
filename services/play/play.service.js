9bba22410bcfcbefa08ff1a3cb25da0c3e5468c9
Edouard Nicolas <edouard.n@lifeaz.fr>
2022-10-14 11:54:06 +0200

    update socket

--- services/play/play.service.js
+++ services/play/play.service.js
@@ -1,5 +1,25 @@
-module.exports.play = (req, res) => {
-    res.send('play');
+const db = require("../../common/db/db");
+module.exports.play = (socket) => {
+    socket.on('start', (data) => {
+       const result = db.client.collection("rooms").find({code: data.code}).toArray().then((result) => {
+            socket.emit('startGame', 10);
+        });
+    });
+
+    socket.on('play', (data) => {
+        const result = db.client.collection("rooms").find({code: data.code}).toArray().then((result) => {
+          if (typeof result[0].score === undefined) {
+              db.client.collection("rooms").updateOne({code: data.code}, {$set: {firstScore: data.score}});
+              socket.emit('wait');
+          }else{
+                if (data.score > result[0].firstScore) {
+                    socket.emit('win');
+                }else {
+                    socket.emit('lose');
+                }
+          }
+        });
+    });
 }
 
 
